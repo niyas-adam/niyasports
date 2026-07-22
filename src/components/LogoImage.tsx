f16bedc2dd08a96295a1abd6d968d221507c1c72
niyas-adam <niyasadam19@gmail.com>
2026-07-22 14:39:12 +0530

    feat: use custom logo PNG and fix LogoImage to use img tag

--- src/components/LogoImage.tsx
+++ src/components/LogoImage.tsx
@@ -1,1 +1,17 @@
-<content of LogoImage.tsx>
+type LogoImageProps = {
+  width?: number;
+  height?: number;
+  className?: string;
+};
+
+export default function LogoImage({ width = 36, height = 36, className }: LogoImageProps) {
+  return (
+    <img
+      src="/logo.png"
+      alt="United Sports"
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
+}
